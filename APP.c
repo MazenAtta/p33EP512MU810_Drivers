be3fae52ed24a956e8aea289d3af1f02775bf253
MazenAtta <mazenatta445@gmail.com>
2025-04-16 16:30:28 +0200

    Updated selected files

--- APP.c
+++ APP.c
@@ -5,33 +5,3 @@
  * Created on February 22, 2025, 6:33 PM
  */
 
-
-#include "APP.h"
-
-
-
-int main() {
-    // Disable all analog pins
-    ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
-
-    RPINR18bits.U1RXR = 75;
-    RPOR0bits.RP64R =1;
-    
-    char received_ch;
-    
-    U1BRG = 468; // (72000000) / (16 ? 9600)? 1
-    U1MODEbits.UARTEN = 1; // enable UART
-    U1STAbits.UTXEN = 1; // enable U1TX (must be after UARTEN)
-
-    
-    while(1){
-        if(U1STAbits.URXDA)
-        {
-            received_ch = U1RXREG;
-            while(!U1STAbits.TRMT);
-                U1TXREG = received_ch;
-        }
-    }
-    
-    return (EXIT_SUCCESS);
-}
